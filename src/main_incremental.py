01cb5e56e3ff26e5c633d529679df956db4bfdd4
Alex Gomez Villa <alviurlex@gmail.com>
2024-04-01 17:30:31 +0200

    Update main_incremental.py

--- src/main_incremental.py
+++ src/main_incremental.py
@@ -10,7 +10,6 @@ import approach
 from loggers.exp_logger import MultiLogger
 from datasets.data_loader import get_loaders
 from datasets.dataset_config import dataset_config
-from last_layer_analysis import last_layer_analysis
 from networks import tvmodels, allmodels, set_tvmodel_head_var
 import random
 import wandb
